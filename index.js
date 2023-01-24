fb49cdfa7ab8f14cabfc2c00468d23edea49a247
Hertzogjr <88346377+Hertzogjr@users.noreply.github.com>
2023-01-24 19:23:43 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,80 +1,134 @@
 const tableBody = document.querySelector("tbody");
+const descriptionInput = document.querySelector('#descriptionInput')
+const valueInput = document.querySelector("#valueInput")
+const typeInput = document.querySelector("#typeInput")
+const include = document.querySelector("#include")
+
 const expensesCard = document.querySelector("#expense-card");
 const incomesCard = document.querySelector("#income-card");
-const saldosCard = document.querySelector("#saldo-card");
+const balanceCard = document.querySelector("#balance-card");
+const body = document.querySelector('body')
 
+
+addEventListener('load', () => descriptionInput.focus()) 
+include.addEventListener("click", () => addTransaction())
 
 let transactions = [];
-transactions = [
-  {
-    description: "card",
-    value: 2000,
-    type: "income",
-  },
-  {
-    description: "mcdonalds",
-    value: 20,
-    type: "expense",
-  },
-  {
-    description: "pao",
-    value: 20,
-    type: "expense",
-  },
-  {
-    description: "açucar",
-    value: 300.05,
-    type: "expense",
-  },
-];
 
-const setItemsDb = () =>
-  localStorage.setItem("transactions", JSON.stringify(transactions));
-const getItemsDb = () => JSON.parse(localStorage.getItem("transactions")) ?? [];
+
+function addTransaction(){
+  if(descriptionInput.value.trim() === "" || valueInput.value === "" || typeInput.value === "")
+    return alert("Fill all inputs")
+
+    const descriptionOutSpaces = descriptionInput.value.trim()
+    const descriptionFormatted = descriptionOutSpaces
+    .split('')[0]
+    .toUpperCase() + descriptionOutSpaces
+    .substring(1) 
+
+  const pureValue = valueInput.value.replace('R$', "").replace('.', '').replace(',', '.')
+
+  const id = transactions[transactions.length - 1]?.id + 1 || 1
+  const description = descriptionFormatted
+  const value = pureValue
+  const type = typeInput.value
+
+  const newTransactions = {
+    id,
+    description,
+    value,
+    type
+  }
+
+  transactions.push(newTransactions)
+
+  setItemsDb()
+  loadItems()
+
+  descriptionInput.value = ""
+  valueInput.value = ""
+};
+
 
 const getTotals = () => {
 
   const amountIncomes = () => getItemsDb().filter(({ type }) => type === "income")
-  const totalIncomes = () => amountIncomes().reduce((acc, { value }) => acc + value ,0)
+  const totalIncomes = () => amountIncomes().reduce((acc, { value }) => acc + Number(value) ,0)
   
   const amountExpenses = () =>
     getItemsDb().filter(({ type }) => type === "expense");
+  const totalExpenses = () => amountExpenses().reduce((acc, {value}) => acc + Number(value), 0)
 
-  const totalExpenses = () => amountExpenses().reduce((acc, {value}) => acc + value, 0)
+  const balance = (totalIncomes() - totalExpenses())
 
-  console.log(typeof totalIncomes)
-  const saldo = (totalIncomes() - totalExpenses())
-  console.log(totalIncomes())
-  console.log(totalExpenses())
-
-
-  incomesCard.innerHTML = `R$ ${totalIncomes()}`
-  expensesCard.innerHTML = `R$ ${totalExpenses()}`
-  saldosCard.innerHTML = `R$ ${saldo}`
-}
-
-getTotals()
-
-
-
-setItemsDb();
-
-const renderItems = () => {
-  getItemsDb().forEach(({ description, value, type }) => {
-    const tr = document.createElement("tr");
-    tr.innerHTML = `
-        <td>${description}</td>
-        <td>${value}</td>
-        <td>${
-          type === "income"
-            ? '<i class="fa-solid fa-arrow-up"></i>'
-            : '<i class="fa-solid fa-arrow-down"></i>'
-        }</td>
-        <td><i class="fa-solid fa-trash"></i></td>       
-    `;
-
-    tableBody.appendChild(tr);
-  });
+  incomesCard.innerHTML = `${formatBrazilianCurrency(totalIncomes())}`
+  expensesCard.innerHTML = `${formatBrazilianCurrency(totalExpenses())}`
+  balanceCard.innerHTML = `${formatBrazilianCurrency(balance)}`
 };
 
-renderItems();
+const deleItemsDb = (index) => {
+    transactions.splice(index, 1)
+    setItemsDb()
+    getItemsDb()
+    loadItems()
+};
+
+const renderItems = ({description, value, type}, index) => {
+  console.log(index)
+  const tr = document.createElement("tr");
+  tr.innerHTML = `  
+      <td>${description}</td>
+      <td>${formatBrazilianCurrency(Number(value))}</td>
+      <td>${
+        type === "income"
+          ? '<i class="fa-solid fa-arrow-up"></i>'
+          : '<i class="fa-solid fa-arrow-down"></i>'
+      }</td>
+      <td><i class="fa-solid fa-trash" id="${index}" onClick=deleItemsDb(id)></i></td>       
+  `;
+  tableBody.appendChild(tr);
+};
+
+const formatBrazilianCurrency = (string) => { return string.toLocaleString('pt-br', {
+  style: "currency",
+  currency: "BRL"
+})
+};
+
+function inputCurrencyMask(event) {
+  const onlyDigits = event.target.value
+    .split("")
+    .filter(s => /\d/.test(s))
+    .join("")
+    .padStart(3, "0")
+  const digitsFloat = onlyDigits.slice(0, -2) + "." + onlyDigits.slice(-2)
+  event.target.value = maskCurrency(digitsFloat)
+}
+
+function maskCurrency(valor, locale = 'pt-BR', currency = 'BRL') {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency
+  }).format(valor)
+}
+
+const loadItems = () => {
+  transactions = getItemsDb()
+  console.log(transactions)
+  tableBody.innerHTML = ''
+  transactions.forEach((transaction, index) => {
+    renderItems(transaction, index)
+  });
+  getTotals()
+};
+
+function setItemsDb(){
+  return localStorage.setItem("transactions", JSON.stringify(transactions)); 
+};
+
+function getItemsDb(){
+  return JSON.parse(localStorage.getItem("transactions")) ?? [];
+};
+
+loadItems()
+
